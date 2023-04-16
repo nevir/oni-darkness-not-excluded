7284363ce7e103e763c63fb39b1e9cdd8685db7c
Ian MacLeod <ian@nevir.net>
2023-04-16 12:18:55 -0700

    Basic initial mod

---
+++ README.md
@@ -0,0 +1,1 @@
+# Darkness Not Excluded
---
+++ src/Mod.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+using KMod;
+using System;
+
+namespace DarknessNotIncluded
+{
+  public class Mod : UserMod2
+  {
+    public override void OnLoad(Harmony harmony)
+    {
+      Console.WriteLine($"Loaded [DarknessNotIncluded]");
+      base.OnLoad(harmony);
+    }
+  }
+}
