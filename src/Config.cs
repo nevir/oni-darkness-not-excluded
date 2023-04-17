b457feacb4509fab894757289c476a4beacc1f2e
Ian MacLeod <ian@nevir.net>
2023-04-17 16:10:41 -0700

    Set up mining lights

--- src/Config.cs
+++ src/Config.cs
@@ -34,11 +34,72 @@ namespace DarknessNotIncluded
     [Option("Lux", "How much light should be emitted around dupes, even when no other light sources are present?", "Dupe Base Glow")]
     public int dupeIntrinsicLightLux { get; set; }
 
-    [Option("Radius", "How much light should be emitted around dupes, even when no other light sources are present?", "Dupe Base Glow")]
+    [Option("Radius", "How far should light be emitted around dupes, even when no other light sources are present?", "Dupe Base Glow")]
     public int dupeIntrinsicLightRadius { get; set; }
 
-    [Option("Color", "What color of light should be emitted around dupes, even when no other light sources are present?", "Dupe Base Glow")]
+    // [Option("Color", "What color of light should be emitted around dupes, even when no other light sources are present?", "Dupe Base Glow")]
+    [JsonIgnore]
     public Color dupeIntrinsicLightColor { get; set; }
+
+    // Tier 1 Mining Hat (Hard Digging)
+
+    [Option("Enabled", "Whether tier 1 mining hats should emit light.", "Tier 1 Mining Hat (Hard Digging)")]
+    public bool miningHatTier1Enabled { get; set; }
+
+    [Option("Lux", "How much light should be emitted from a tier 1 mining hat?", "Tier 1 Mining Hat (Hard Digging)")]
+    public int miningHatTier1Lux { get; set; }
+
+    [Option("Radius", "How far should light be emitted from a tier 1 mining hat?", "Tier 1 Mining Hat (Hard Digging)")]
+    public int miningHatTier1Radius { get; set; }
+
+    // [Option("Color", "What color should a tier 1 mining hat emit?", "Tier 1 Mining Hat (Hard Digging)")]
+    [JsonIgnore]
+    public Color miningHatTier1Color { get; set; }
+
+    // Tier 2 Mining Hat (2—Superhard Digging)
+
+    [Option("Enabled", "Whether tier 2 mining hats should emit light.", "Tier 2 Mining Hat (2—Superhard Digging)")]
+    public bool miningHatTier2Enabled { get; set; }
+
+    [Option("Lux", "How much light should be emitted from a tier 2 mining hat?", "Tier 2 Mining Hat (2—Superhard Digging)")]
+    public int miningHatTier2Lux { get; set; }
+
+    [Option("Radius", "How far should light be emitted from a tier 2 mining hat?", "Tier 2 Mining Hat (2—Superhard Digging)")]
+    public int miningHatTier2Radius { get; set; }
+
+    // [Option("Color", "What color should a tier 2 mining hat emit?", "Tier 2 Mining Hat (2—Superhard Digging)")]
+    [JsonIgnore]
+    public Color miningHatTier2Color { get; set; }
+
+    // Tier 3 Mining Hat (Super-Duperhard Digging)
+
+    [Option("Enabled", "Whether tier 3 mining hats should emit light.", "Tier 3 Mining Hat (Super-Duperhard Digging)")]
+    public bool miningHatTier3Enabled { get; set; }
+
+    [Option("Lux", "How much light should be emitted from a tier 3 mining hat?", "Tier 3 Mining Hat (Super-Duperhard Digging)")]
+    public int miningHatTier3Lux { get; set; }
+
+    [Option("Radius", "How far should light be emitted from a tier 3 mining hat?", "Tier 3 Mining Hat (Super-Duperhard Digging)")]
+    public int miningHatTier3Radius { get; set; }
+
+    // [Option("Color", "What color should a tier 3 mining hat emit?", "Tier 3 Mining Hat (Super-Duperhard Digging)")]
+    [JsonIgnore]
+    public Color miningHatTier3Color { get; set; }
+
+    // Tier 4 Mining Hat (Hazmat Digging)
+
+    [Option("Enabled", "Whether tier 4 mining hats should emit light.", "Tier 4 Mining Hat (Hazmat Digging)")]
+    public bool miningHatTier4Enabled { get; set; }
+
+    [Option("Lux", "How much light should be emitted from a tier 4 mining hat?", "Tier 4 Mining Hat (Hazmat Digging)")]
+    public int miningHatTier4Lux { get; set; }
+
+    [Option("Radius", "How far should light be emitted from a tier 4 mining hat?", "Tier 4 Mining Hat (Hazmat Digging)")]
+    public int miningHatTier4Radius { get; set; }
+
+    // [Option("Color", "What color should a tier 4 mining hat emit?", "Tier 4 Mining Hat (Hazmat Digging)")]
+    [JsonIgnore]
+    public Color miningHatTier4Color { get; set; }
 
     public Config()
     {
@@ -53,6 +114,30 @@ namespace DarknessNotIncluded
       dupeIntrinsicLightLux = 200;
       dupeIntrinsicLightRadius = 2;
       dupeIntrinsicLightColor = Color.white;
+
+      // Tier 1 Mining Hat (Hard Digging)
+      miningHatTier1Enabled = true;
+      miningHatTier1Lux = 600;
+      miningHatTier1Radius = 3;
+      miningHatTier1Color = TUNING.LIGHT2D.LIGHT_YELLOW;
+
+      // Tier 2 Mining Hat (2—Superhard Digging)
+      miningHatTier2Enabled = true;
+      miningHatTier2Lux = 800;
+      miningHatTier2Radius = 4;
+      miningHatTier2Color = TUNING.LIGHT2D.LIGHT_YELLOW;
+
+      // Tier 3 Mining Hat (Super-Duperhard Digging)
+      miningHatTier3Enabled = true;
+      miningHatTier3Lux = 1400;
+      miningHatTier3Radius = 5;
+      miningHatTier3Color = Color.white;
+
+      // Tier 4 Mining Hat (Hazmat Digging)
+      miningHatTier4Enabled = true;
+      miningHatTier4Lux = 1800;
+      miningHatTier4Radius = 6;
+      miningHatTier4Color = Color.white;
     }
 
     [HarmonyPatch(typeof(Game)), HarmonyPatch("OnPrefabInit")]
--- src/MinionLighting.cs
+++ src/MinionLighting.cs
@@ -9,6 +9,7 @@ namespace DarknessNotIncluded
     static void Postfix(GameObject __result)
     {
       __result.AddOrGet<MinionIntrinsicLight>();
+      __result.AddOrGet<MinionMinerHatLight>();
     }
   }
 
@@ -39,6 +40,76 @@ namespace DarknessNotIncluded
       LegLight.Lux = config.dupeIntrinsicLightLux / 2;
       LegLight.shape = LightShape.Circle;
       LegLight.enabled = config.dupeIntrinsicLightEnabled;
+    }
+  }
+
+  class MinionMinerHatLight : KMonoBehaviour, ISim33ms
+  {
+    [MyCmpGet]
+    private MinionIdentity _minionIdentity;
+
+    public Light2D Light { get; set; }
+
+    private string currentHat;
+
+    protected override void OnPrefabInit()
+    {
+      var resume = _minionIdentity.GetComponent<MinionResume>();
+      currentHat = resume.CurrentHat;
+
+      Light = _minionIdentity.gameObject.AddComponent<Light2D>();
+      Light.Offset = new Vector2(0f, 1.0f);
+      Light.IntensityAnimation = 1f;
+      UpdateLightConfig();
+    }
+
+    public void Sim33ms(float dt)
+    {
+      var hat = _minionIdentity.GetComponent<MinionResume>().CurrentHat;
+      if (hat == currentHat) return;
+
+      currentHat = hat;
+      UpdateLightConfig();
+    }
+
+    private void UpdateLightConfig()
+    {
+      var config = Config.Instance;
+
+      switch (currentHat)
+      {
+        case "hat_role_mining1":
+          Light.enabled = config.miningHatTier1Enabled;
+          Light.Lux = config.miningHatTier1Lux;
+          Light.Range = config.miningHatTier1Radius;
+          Light.Color = config.miningHatTier1Color;
+          break;
+
+        case "hat_role_mining2":
+          Light.enabled = config.miningHatTier2Enabled;
+          Light.Lux = config.miningHatTier2Lux;
+          Light.Range = config.miningHatTier2Radius;
+          Light.Color = config.miningHatTier2Color;
+          break;
+
+        case "hat_role_mining3":
+          Light.enabled = config.miningHatTier3Enabled;
+          Light.Lux = config.miningHatTier3Lux;
+          Light.Range = config.miningHatTier3Radius;
+          Light.Color = config.miningHatTier3Color;
+          break;
+
+        case "hat_role_mining4":
+          Light.enabled = config.miningHatTier4Enabled;
+          Light.Lux = config.miningHatTier4Lux;
+          Light.Range = config.miningHatTier4Radius;
+          Light.Color = config.miningHatTier4Color;
+          break;
+
+        default:
+          Light.enabled = false;
+          break;
+      }
     }
   }
 }
