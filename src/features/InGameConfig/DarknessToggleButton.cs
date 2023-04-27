180ff1bfc318dfa8847acb8739a38d79008af6db
Ian MacLeod <ian@nevir.net>
2023-04-27 16:28:32 -0700

    Capture scrolling for in-game options

--- src/features/InGameConfig/DarknessToggleButton.cs
+++ src/features/InGameConfig/DarknessToggleButton.cs
@@ -23,36 +23,49 @@ namespace DarknessNotIncluded.InGameConfig
           SpriteSize = new Vector2(24, 24),
           Margin = new RectOffset(3, 3, 3, 3),
           Color = PUITuning.Colors.ButtonBlueStyle,
-          OnClick = (source) =>
-          {
-            Darkness.Behavior.enabled = !Darkness.Behavior.enabled;
-
-            var kButton = source.GetComponent<KButton>();
-            kButton.GetComponent<ToolTip>().SetSimpleTooltip(Darkness.Behavior.enabled ? DARKNESS_ACTIVE_TOOLTIP : DARKNESS_DISABLED_TOOLTIP);
-            kButton.bgImage.colorStyleSetting = Darkness.Behavior.enabled ? PUITuning.Colors.ButtonBlueStyle : PUITuning.Colors.ButtonPinkStyle;
-            kButton.fgImage.sprite = Assets.GetSprite(Darkness.Behavior.enabled ? "icon_category_lights_disabled" : "icon_category_lights");
-          },
         }
         .AddOnRealize((realized) =>
         {
           var kButton = realized.GetComponent<KButton>();
           kButton.onBtnClick += (btn) =>
           {
-            if (btn != KKeyCode.Mouse1) return;
-
-            var OptionsDialog = Type.GetType("PeterHan.PLib.Options.OptionsDialog");
-            var dialog = OptionsDialog.GetConstructor(BindingFlags.NonPublic | BindingFlags.Instance, null, new Type[] { typeof(Type) }, null).Invoke(new object[] { typeof(Config) });
-
-            Action<object> onClose = (object config) =>
+            if (btn == KKeyCode.Mouse0)
             {
-              Config.Instance = config as Config;
-            };
-            OptionsDialog.GetProperty("OnClose").SetValue(dialog, onClose);
-
-            OptionsDialog.GetMethod("ShowDialog").Invoke(dialog, BindingFlags.NonPublic, null, new object[] { }, null);
+              ToggleDarkness(kButton);
+            }
+            else if (btn == KKeyCode.Mouse1)
+            {
+              ShowOptions();
+            }
           };
         });
         button.AddTo(___secondaryRow.gameObject);
+      }
+
+      static void ToggleDarkness(KButton button)
+      {
+        Darkness.Behavior.enabled = !Darkness.Behavior.enabled;
+
+        button.GetComponent<ToolTip>().SetSimpleTooltip(Darkness.Behavior.enabled ? DARKNESS_ACTIVE_TOOLTIP : DARKNESS_DISABLED_TOOLTIP);
+        button.bgImage.colorStyleSetting = Darkness.Behavior.enabled ? PUITuning.Colors.ButtonBlueStyle : PUITuning.Colors.ButtonPinkStyle;
+        button.fgImage.sprite = Assets.GetSprite(Darkness.Behavior.enabled ? "icon_category_lights_disabled" : "icon_category_lights");
+      }
+
+      static void ShowOptions()
+      {
+        var OptionsDialog = Type.GetType("PeterHan.PLib.Options.OptionsDialog");
+        var dialog = OptionsDialog.GetConstructor(BindingFlags.NonPublic | BindingFlags.Instance, null, new Type[] { typeof(Type) }, null).Invoke(new object[] { typeof(Config) });
+
+        Action<object> onClose = (object config) =>
+        {
+          Config.Instance = config as Config;
+        };
+        OptionsDialog.GetProperty("OnClose").SetValue(dialog, onClose);
+
+        OptionsDialog.GetMethod("ShowDialog").Invoke(dialog, BindingFlags.NonPublic, null, new object[] { }, null);
+
+        var screen = (KScreen)OptionsDialog.GetField("dialog", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(dialog);
+        screen.ConsumeMouseScroll = true;
       }
     }
   }
